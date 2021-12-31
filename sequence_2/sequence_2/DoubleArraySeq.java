18021ee21fb50f31fd12598361c2e5b7605c6fd9
matthewschwartz <matthewschwartz@192.168.0.5>
2021-12-31 00:41:30 -0600

    f

--- sequence_2/sequence_2/DoubleArraySeq.java
+++ sequence_2/sequence_2/DoubleArraySeq.java
@@ -1,5 +1,14 @@
 package sequence_2;
 
 public class DoubleArraySeq {
-
+	
+	double[] data;
+	int manyItems;
+	int currentIndex;
+	
+	public DoubleArraySeq() {
+		this.data = new double[10];
+		this.manyItems = 0;
+		this.currentIndex = 0;
+	}
 }
